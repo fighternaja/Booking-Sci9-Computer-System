45175d65f386532814f7f9ff7b181b5d4e3a2281
Panuwich Thammawong <fighterkab123@gmail.com>
2026-02-05 16:37:57 +0700

    Remove Schedule from console.php (already in Kernel.php)

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,25 +2,9 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
-use App\Http\Controllers\Api\WaitlistController;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Schedule tasks
-Schedule::command('bookings:auto-cancel')
-    ->everyMinute()
-    ->withoutOverlapping();
-
-Schedule::command('bookings:send-reminders')
-    ->everyFiveMinutes()
-    ->withoutOverlapping();
-
-Schedule::call(function () {
-    WaitlistController::processWaitlists();
-})
-    ->name('process-waitlists')
-    ->everyFiveMinutes()
-    ->withoutOverlapping();
+// Schedule tasks are moved to app/Console/Kernel.php
